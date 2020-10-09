cf77567a8039cb1b35cb8d4e2bec299d8b36f792
Sri Madhan M <22276366+srimadhan11@users.noreply.github.com>
2020-10-09 23:41:44 +0530

    improved code readability, added nfg_formator function

--- Q4.py
+++ Q4.py
@@ -1,3 +1,5 @@
+import os
+import os.path
 import itertools
 import numpy as np
 
@@ -5,44 +7,69 @@ from EFGGameParser import *
 
 
 def Q4(filename):
+    # to format the payoff value for every strategy set
+    def payoff_formator(elm):
+        return ' '.join(map(str, elm))
+
+    # to format the game representation
+    def nfg_formator(name, players, strategies, payoff_matrix):
+        return [
+            f'NFG 1 R "{name}"',
+            '{{ "{}" }} {{ {} }}'.format('" "'.join(map(str, players)), ' '.join(map(str, strategies))),
+            ' '.join(map(str, payoff_matrix))
+        ]
+
     game = EfgGameParser.parse_efg_file(filename)
-    players_strategies = tuple(tuple(itertools.product(*(tuple(info_set.action_names for info_sets in player.info_sets for info_set in info_sets)))) for player in game.players)
-    players_info_set = tuple(tuple(info_set.info_num for info_sets in player.info_sets for info_set in info_sets) for player in game.players)
-    n_dims = tuple(len(strategies) for strategies in players_strategies)
+    strategy_profile_set, players_info_set, players_name = list(), list(), list()
+    for player in game.players:
+        t_action_names, t_players_info_set = list(), list()
+        for info_sets in player.info_sets:
+            for info_set in info_sets:
+                t_action_names.append(info_set.action_names)
+                t_players_info_set.append(info_set.info_num)
+        players_name.append(player.name)
+        strategy_profile_set.append(tuple(itertools.product(*t_action_names)))
+        players_info_set.append(t_players_info_set)
+    n_dims = tuple(len(strategies) for strategies in strategy_profile_set)
 
-    def get_child_node(owner_player, info_num, action):
-        for node in game.prefix_traversal:
-            if (isinstance(node, Personal_node)) \
-                    and node.owner_player == owner_player and node.info_num == info_num:
-                for child in node.children:
-                    if action == child[0]:
-                        if (isinstance(child[1], Personal_node)):
-                            return 'p', child[1].owner_player, child[1].info_num
-                        if (isinstance(child[1], Terminal_node)):
-                            return 't', None, child[1].payoffs
-        return None, None, None
+    def find_payoff(payoff_index):
+        strategy_profile = tuple(strategy_profile_set[p_idx][s_idx] for p_idx,s_idx in enumerate(payoff_index))
+        players_infonum_to_action = tuple(
+                dict(
+                    tuple(zip(info_nums, strategy_profile[p_idx]))
+                ) for p_idx,info_nums in enumerate(players_info_set)
+            )
 
-    def compute_payoff(payoff_index):
-        player_wise_strategies = tuple(players_strategies[p_idx][s_idx] for p_idx,s_idx in enumerate(payoff_index))
-        player_wise_info_num_to_action = tuple(dict(tuple(zip(info_nums, player_wise_strategies[p_idx]))) for p_idx,info_nums in enumerate(players_info_set))
-
-        node_type, owner_player, info_num = 'p', game.prefix_traversal[0].owner_player, game.prefix_traversal[0].info_num
-        while node_type == 'p':
+        payoffs = None
+        owner_player, info_num, node = game.prefix_traversal[0].owner_player, game.prefix_traversal[0].info_num, game.prefix_traversal[0]
+        while node:
             zero_idx = owner_player - 1     # on the assumption that players are indexed from 1
-            action = player_wise_info_num_to_action[zero_idx][info_num]
-            node_type, owner_player, info_num = get_child_node(owner_player, info_num, action)
-        assert node_type == 't', "node_type should be terminal node"
-        return info_num     # incase of node_type == 't', info_num has payoff
+            action = players_infonum_to_action[zero_idx][info_num]
+            for child in node.children:
+                if action != child[0]:
+                    continue
+                if (isinstance(child[1], Personal_node)):
+                    owner_player, info_num, node = child[1].owner_player, child[1].info_num, child[1]
+                if (isinstance(child[1], Terminal_node)):
+                    payoffs, node = child[1].payoffs, None      # None is used to end loop
+                break
+            else:
+                print('ERR: no valid child has found')
+        assert payoffs, "payoffs should not be empty/None"
+        return payoffs
 
     N = np.empty(n_dims, dtype=object)
     with np.nditer(N, flags=['multi_index', 'refs_ok'], op_flags=['writeonly']) as it:
         for elm in it:
-            elm[...] = ' '.join(map(str, compute_payoff(it.multi_index)))
-    return N.reshape(-1, order='F')
+            elm[...] = payoff_formator(find_payoff(it.multi_index))
+    return nfg_formator(game.game_name, players_name, n_dims, N.reshape(-1, order='F').tolist())
 
 
-print(Q4('EfgTestCases/test_case_1'))
-print(Q4('EfgTestCases/test_case_2'))
-print(Q4('EfgTestCases/test_case_3'))   # information_set is not linearly increasing, will produce wrong result
-print(Q4('EfgTestCases/test_case_4'))
+dirname = 'EfgTestCases'
+for filename in os.listdir(dirname):
+    try:
+        payoffs = Q4(os.path.join(dirname, filename))
+        print(f'{filename}:\n{payoffs}\n')
+    except Exception as exp:
+        print(exp)
 
